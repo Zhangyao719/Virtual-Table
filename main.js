fc9fd683c2d981b17312f903e088c68d9ecee171
Zhangyao719 <1258858403@qq.com>
2022-07-23 23:57:00 +0800

    feat: realize virtual scrolling

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
 const RootComponent = {
     name: 'app',
     setup() {
-        const { ref, computed} = Vue
-        const rowHeight = ref(55) // 行高
-        const n = ref(10) // 可视区域展示条数
-        const data = ref(Array.from({ length: 100 }, (_, index) => index + 1)) // 模拟数据(100条)
+        const { ref, computed } = Vue
 
-        const contentHeight = computed(() => {
-            return data.value.length * rowHeight.value
-        })
+        /* 前置配置 */
+        const rowHeight = ref(55) // 设置行高
+        const limit = ref(5) // 设置可视区域展示条数
+        const data = ref(Array.from({ length: 100 }, (_, index) => ({
+            index,
+            title: `第${index + 1}条数据`
+        }))) // 模拟数据(100条)
+        const total = computed(() => data.value.length)
+        const contentHeight = computed(() => total.value * rowHeight.value)
+        
+        /* 可视区域 起始下标初始值 */
+        const originStartIndex = ref(0)
+        const startIndex = ref(0)
+        const endIndex = ref(limit.value)
+
+        // 可视区域数据（根据startIndex, endIndex动态截取）
+        const contentData = computed(() => data.value.slice(startIndex.value, endIndex.value))
 
         const onScroll = e => {
-            console.log(' ~ e', e.target.scrollTop)
+            const currentIndex = Math.floor(e.target.scrollTop / rowHeight.value)
+            if (originStartIndex.value !== currentIndex) {
+                console.log(222);
+                originStartIndex.value = currentIndex
+                startIndex.value = currentIndex
+                endIndex.value = Math.min(currentIndex + limit.value, total.value - 1)
+                console.log(startIndex.value, endIndex.value, contentData.value);
+            }
         }
 
         return {
             rowHeight,
-            n,
+            limit,
             data,
+            total,
             contentHeight,
+            contentData,
             onScroll
         }
     }
